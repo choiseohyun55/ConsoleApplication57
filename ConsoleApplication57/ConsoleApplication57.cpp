51ccbb0a6d4100f15d4dbe010751a244c6de4ac1
서현 최 <qkr7576@naver.com>
2020-06-05 11:52:24 +0900

    초기 커밋.

---
+++ ConsoleApplication57/ConsoleApplication57.cpp
@@ -0,0 +1,18 @@
+﻿
+#include <iostream>
+using namespace std;
+int main() {
+	int arr[7] = { 1, 4, 3, 3, 5, 7, 6 };
+	int* front = &arr[0];
+	int* back = &arr[6];
+
+	for (int i = 0; i < 3; i++) {
+		int num = *(back - i);
+		*(back - i) = *(front + i);
+		*(front + i) = num;
+	}
+	for (int i = 0; i < 7; i++) {
+		cout << arr[i] << " ";
+	}
+}
+
